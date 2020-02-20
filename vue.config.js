3d7ce2bb5299d4ab630360498b50ea08500c044e
shs3832 <shs3832@naver.com>
2020-02-20 13:26:26 +0900

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
     devServer: {
         overlay: {
@@ -14,20 +13,3 @@ module.exports = {
         }
     }
 };
-=======
-module.exports = {
-    devServer: {
-        overlay: {
-            warnings: false,
-            errors: false
-        }
-    },
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `@import "@/assets/css/global.scss";`
-            }
-        }
-    }
-};
->>>>>>> 1a0744b4a7633898d4c83c0d96a8f2b8881bc2e8
